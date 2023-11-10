de2a0ee535d5f65da364bb6226ed2bb98e3963f7
Steven Adi Santoso <sstevenadi@gmail.com>
2023-11-10 16:35:30 +0700

    first commit

---
+++ src/main/java/com/silvering/tutorialscrud/TutorialsCrudApplication.java
@@ -0,0 +1,13 @@
+package com.silvering.tutorialscrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialsCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialsCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/silvering/tutorialscrud/TutorialsCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silvering.tutorialscrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialsCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
